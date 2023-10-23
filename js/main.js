7b9a05ab7b46cda92ae44490b58014ca91babced
cartobaldrica <baldrica18@gmail.com>
2023-10-23 10:50:50 -0500

    update font add second basemap

--- js/main.js
+++ js/main.js
@@ -265,7 +265,7 @@ Add audio autoplay element
         //map.compassBearing.enable();
         map.setBearing(90);
         //load custom basemap created using QTiles        
-        let baseLayer = L.tileLayer('data/basemap_light4/{z}/{x}/{y}.jpg', {
+        let baseLayer = L.tileLayer('data/basemap_light5/{z}/{x}/{y}.jpg', {
             minZoom:minZoom,
             maxZoom:maxZoom,
             maxBounds:bounds,       
